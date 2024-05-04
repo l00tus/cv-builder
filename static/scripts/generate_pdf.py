3fc6eb861e0febc5a30bda47266b70d38660b7e6
l00tus <marioalexandruu04@gmail.com>
2024-05-04 21:43:46 +0300

    Generate PDF file for the CV

---
+++ static/scripts/generate_pdf.py
@@ -0,0 +1,15 @@
+import os
+
+def generate_pdf():
+    tex_path = 'generated/cv.tex'
+    pdf_path = 'generated/cv.pdf'
+    
+    try:
+        command = 'pdflatex -output-directory=' + os.path.dirname(pdf_path) + ' ' + tex_path
+        os.system(command)
+        os.remove(tex_path[:-3] + 'aux')
+        os.remove(tex_path[:-3] + 'log')
+        os.remove(tex_path[:-3] + 'out')
+    except Exception as e:
+        print(e)
+    
